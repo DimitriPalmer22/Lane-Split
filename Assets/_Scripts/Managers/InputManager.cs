679f9ac82fca9aa0a169437df77a7094aaf2c74a
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-09-16 14:39:00 -0400

    Swipe Direction Detected

--- Assets/_Scripts/Managers/InputManager.cs
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -8,8 +8,22 @@ using UnityEngine.InputSystem.EnhancedTouch;
 
 public class InputManager : MonoBehaviour, IDebugManaged
 {
+    public enum SwipeDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        UpLeft,
+        UpRight,
+        DownLeft,
+        DownRight
+    }
+    
     public static InputManager Instance { get; private set; }
     public PlayerControls PlayerControls { get; private set; }
+
+    public event Action<Vector2, SwipeDirection> onSwipe;
 
     #region Swipe Detection
 
@@ -24,6 +38,8 @@ public class InputManager : MonoBehaviour, IDebugManaged
     private Vector2 _swipe;
 
     #endregion
+
+    #region Unity Functions
 
     private void Awake()
     {
@@ -63,6 +79,14 @@ public class InputManager : MonoBehaviour, IDebugManaged
         PlayerControls.Disable();
     }
 
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    #endregion
+
     #region Input Functions
 
     private void InitializeInput()
@@ -101,27 +125,60 @@ public class InputManager : MonoBehaviour, IDebugManaged
         var differenceDistance = difference.magnitude;
 
         // TODO: Implement a system for making some swipes invalid
+        // Resolution-based?
 
         // Set the swipe vector
         _swipe = difference;
 
-        // TODO: Call an event to notify other classes that a swipe has been detected
+        // Call an event to notify other classes that a swipe has been detected
+        onSwipe?.Invoke(_swipe, DetermineDirection(_swipe));
 
         // TODO: When registering swipes, use dot product to determine which direction a swipe is in
     }
 
     #endregion
 
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     public string GetDebugText()
     {
         return $"Is Swiping: {_isSwiping}\n" +
                $"Touch Position: {_currentTouchPosition}\n" +
                $"Swipe: {_swipe}\n";
+    }
+
+    private static SwipeDirection DetermineDirection(Vector2 obj)
+    {
+        var up = Vector2.up;
+        var down = Vector2.down;
+        var left = Vector2.left;
+        var right = Vector2.right;
+        var upLeft = (Vector2.up + Vector2.left).normalized;
+        var upRight = (Vector2.up + Vector2.right).normalized;
+        var downLeft = (Vector2.down + Vector2.left).normalized;
+        var downRight = (Vector2.down + Vector2.right).normalized;
+
+        var upDot = Vector2.Dot(obj.normalized, up);
+        var downDot = Vector2.Dot(obj.normalized, down);
+        var leftDot = Vector2.Dot(obj.normalized, left);   
+        var rightDot = Vector2.Dot(obj.normalized, right);
+        var upLeftDot = Vector2.Dot(obj.normalized, upLeft);
+        var upRightDot = Vector2.Dot(obj.normalized, upRight);
+        var downLeftDot = Vector2.Dot(obj.normalized, downLeft);
+        var downRightDot = Vector2.Dot(obj.normalized, downRight);
+        
+        // Add all the dots to a sorted list
+        var dots = new SortedList<float, SwipeDirection>
+        {
+            {upDot, SwipeDirection.Up},
+            {downDot, SwipeDirection.Down},
+            {leftDot, SwipeDirection.Left},
+            {rightDot, SwipeDirection.Right},
+            {upLeftDot, SwipeDirection.UpLeft},
+            {upRightDot, SwipeDirection.UpRight},
+            {downLeftDot, SwipeDirection.DownLeft},
+            {downRightDot, SwipeDirection.DownRight}
+        };
+        
+        // Return the largest dot
+        return dots[dots.Keys[dots.Count - 1]];
     }
 }
--- Assets/_Scripts/TestPlayerScript.cs
+++ Assets/_Scripts/TestPlayerScript.cs
@@ -4,12 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class TestPlayerScript : MonoBehaviour
+public class TestPlayerScript : MonoBehaviour, IDebugManaged
 {
+    [SerializeField] private float moveDistance = 10f;
+
+    private String debugString = "";
+    
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize the input
+        InputManager.Instance.onSwipe += OnSwipe;
+        
+        // Add this object to the debug manager
+        DebugManager.Instance.AddDebugItem(this);
     }
+
+    private void OnDestroy()
+    {
+        // Remove this object from the debug manager
+        InputManager.Instance.onSwipe -= OnSwipe;
+        
+        // Remove this object from the debug manager
+        DebugManager.Instance.RemoveDebugItem(this);
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -19,7 +38,43 @@ public class TestPlayerScript : MonoBehaviour
 
     #region Input Functions
 
+    private void OnSwipe(Vector2 obj, InputManager.SwipeDirection direction)
+    {
+        
+        var up = Vector2.up;
+        var down = Vector2.down;
+        var left = Vector2.left;
+        var right = Vector2.right;
+        var upLeft = (Vector2.up + Vector2.left).normalized;
+        var upRight = (Vector2.up + Vector2.right).normalized;
+        var downLeft = (Vector2.down + Vector2.left).normalized;
+        var downRight = (Vector2.down + Vector2.right).normalized;
 
+        var upDot = Vector2.Dot(obj.normalized, up);
+        var downDot = Vector2.Dot(obj.normalized, down);
+        var leftDot = Vector2.Dot(obj.normalized, left);   
+        var rightDot = Vector2.Dot(obj.normalized, right);
+        var upLeftDot = Vector2.Dot(obj.normalized, upLeft);
+        var upRightDot = Vector2.Dot(obj.normalized, upRight);
+        var downLeftDot = Vector2.Dot(obj.normalized, downLeft);
+        var downRightDot = Vector2.Dot(obj.normalized, downRight);
+        
+        debugString = $"Player Swipe Dots:\n" +
+                      $"Up: {upDot}\n" +
+                      $"Down: {downDot}\n" +
+                      $"Left: {leftDot}\n" +
+                      $"Right: {rightDot}\n" +
+                      $"UpLeft: {upLeftDot}\n" +
+                      $"UpRight: {upRightDot}\n" +
+                      $"DownLeft: {downLeftDot}\n" +
+                      $"DownRight: {downRightDot}\n" +
+                      $"ACTUAL Direction: {direction}\n";
+    }
 
     #endregion
+
+    public string GetDebugText()
+    {
+        return debugString;
+    }
 }
