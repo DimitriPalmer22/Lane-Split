9aa554d3398b41bd29faf8e278a25f23602926bd
Andre Powell <andre.r.powell@knights.ucf.edu>
2024-10-23 16:40:38 -0400

    comments

--- Assets/_Scripts/MenuController.cs
+++ Assets/_Scripts/MenuController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+// This script is used to control the menu buttons
 public class MenuController : MonoBehaviour
 {
+    // Loads Play scene
    public void LoadScene(string sceneName)
    {
        SceneManager.LoadScene(sceneName);
