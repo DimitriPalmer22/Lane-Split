3aa5af3cf9bf72c0ffb2fd993a6215639b821b06
Andre Powell <andre.r.powell@knights.ucf.edu>
2024-11-07 00:06:25 -0500

    Added: Camera Switching in tunnels

---
+++ Assets/_Scripts/MechanicsPrototype/CameraSwitchHandler.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+
+public class CameraSwitchHandler : MonoBehaviour
+{
+    
+    public CinemachineVirtualCamera currentCamera;
+    public CinemachineVirtualCamera otherCamera;
+    
+    private void Start()
+    {
+        // Set the current camera to the highest priority
+        currentCamera.Priority = 20;
+        
+        // Set the other camera to the lowest priority
+        otherCamera.Priority = 10;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("SwitchCamera"))
+            return;
+
+        SwitchPriority();
+    }
+
+    private void SwitchPriority()
+    {
+        // Switch the camera priority
+        var temp = currentCamera.Priority;
+        currentCamera.Priority = otherCamera.Priority;
+        otherCamera.Priority = temp;
+    }
+   
+}
