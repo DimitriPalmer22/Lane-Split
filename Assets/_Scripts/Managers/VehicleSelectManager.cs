a1bf84226f59c218553699350a00b61d7a46ac2b
Andre Powell <andre.r.powell@knights.ucf.edu>
2024-12-01 19:18:48 -0500

    Added: Viper Mats and Vehicle Customization

--- Assets/_Scripts/Managers/VehicleSelectManager.cs
+++ Assets/_Scripts/Managers/VehicleSelectManager.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 
 public class VehicleSelectManager : MonoBehaviour
 {
     #region Serialized Fields
 
-    // Array to hold vehicle prefabs
+    // Array to hold vehicle prefabs and their info
     [SerializeField] private VehicleSelectionInfo[] vehiclePrefabs;
 
     [SerializeField] [Min(0)] private float rotationsPerSecond;
@@ -22,16 +19,27 @@ public class VehicleSelectManager : MonoBehaviour
     // Reference to the current vehicle instance
     private GameObject _currentVehicleInstance;
 
+    // Array to hold all renderers of the current vehicle
+    private Renderer[] _renderers;
+
     // Variable to store the selected vehicle index
     private int _selectedVehicleIndex;
 
     // The current rotation of the vehicle around the y-axis
     private float _currentRotation;
 
+    // Current material index for the selected vehicle
+    private int _currentMaterialIndex;
+
+    // Current vehicle's materials
+    private Material[] _currentVehicleMaterials;
+
     #endregion
 
     private void Start()
     {
+        _currentMaterialIndex = 0;
+
         // Input manager to subscribe to the swipe event
         InputManager.Instance.OnSwipe += OnSwipeSelectVehicle;
 
@@ -55,6 +63,11 @@ public class VehicleSelectManager : MonoBehaviour
             case InputManager.SwipeDirection.Up:
                 LoadSelectedVehicle();
                 break;
+
+            case InputManager.SwipeDirection.Down:
+                // Change the material of the vehicle when the player swipes down
+                ChangeVehicleMaterial();
+                break;
         }
     }
 
@@ -77,10 +90,7 @@ public class VehicleSelectManager : MonoBehaviour
     private void SelectPreviousVehicle()
     {
         // Cycle through the vehicles
-        _selectedVehicleIndex = (_selectedVehicleIndex - 1) % vehiclePrefabs.Length;
-
-        if (_selectedVehicleIndex < 0)
-            _selectedVehicleIndex += vehiclePrefabs.Length;
+        _selectedVehicleIndex = (_selectedVehicleIndex - 1 + vehiclePrefabs.Length) % vehiclePrefabs.Length;
 
         InstantiateSelectedVehicle();
     }
@@ -101,9 +111,31 @@ public class VehicleSelectManager : MonoBehaviour
         if (_currentVehicleInstance != null)
             Destroy(_currentVehicleInstance);
 
+        // Reset material index for the new vehicle
+        _currentMaterialIndex = 0;
+
+        // Get the selected vehicle info
+        var selectedVehicleInfo = vehiclePrefabs[_selectedVehicleIndex];
+
         // Instantiate the selected vehicle prefab and store the instance
         _currentVehicleInstance =
-            Instantiate(vehiclePrefabs[_selectedVehicleIndex].DisplayPrefab, transform.position, transform.rotation);
+            Instantiate(selectedVehicleInfo.DisplayPrefab, transform.position, transform.rotation);
+
+        // Get all renderers from the instantiated vehicle
+        _renderers = _currentVehicleInstance.GetComponentsInChildren<Renderer>();
+
+        // Set the current vehicle's materials
+        _currentVehicleMaterials = selectedVehicleInfo.VehicleMaterials;
+
+        // Apply the current material to the new vehicle
+        if (_currentVehicleMaterials != null && _currentVehicleMaterials.Length > 0)
+        {
+            ApplyMaterialToVehicle(_currentVehicleMaterials[_currentMaterialIndex]);
+        }
+        else
+        {
+            Debug.LogWarning("No materials assigned for the selected vehicle.");
+        }
     }
 
     // Function to load the selected vehicle
@@ -116,13 +148,48 @@ public class VehicleSelectManager : MonoBehaviour
         SceneManager.LoadScene("DimitriScene");
     }
 
+    // Function to change the vehicle's material
+    private void ChangeVehicleMaterial()
+    {
+        if (_currentVehicleMaterials == null || _currentVehicleMaterials.Length == 0)
+        {
+            Debug.LogWarning("No materials available to change.");
+            return;
+        }
+
+        // Cycle through the materials
+        _currentMaterialIndex = (_currentMaterialIndex + 1) % _currentVehicleMaterials.Length;
+
+        // Apply the new material
+        ApplyMaterialToVehicle(_currentVehicleMaterials[_currentMaterialIndex]);
+    }
+
+    // Function to apply a material to all renderers of the vehicle
+    private void ApplyMaterialToVehicle(Material newMaterial)
+    {
+        if (newMaterial == null)
+        {
+            Debug.LogWarning("Material is null.");
+            return;
+        }
+
+        foreach (var renderer in _renderers)
+        {
+            renderer.material = newMaterial;
+        }
+    }
+
     [Serializable]
     private class VehicleSelectionInfo
     {
         [SerializeField] private GameObject displayPrefab;
         [SerializeField] private GameObject gameVehiclePrefab;
+        [SerializeField] private Material[] vehicleMaterials;
 
         public GameObject DisplayPrefab => displayPrefab;
+
         public GameObject GameVehiclePrefab => gameVehiclePrefab;
+
+        public Material[] VehicleMaterials => vehicleMaterials;
     }
 }
